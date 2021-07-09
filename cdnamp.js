0e7c40f3af10ff1ba90cc142b345c54cb6f723c0
Furqancorbin <60383939+furqancorbin@users.noreply.github.com>
2021-07-09 11:47:22 +0500

    Create cdnamp.js

---
+++ cdnamp.js
@@ -0,0 +1,18 @@
+
+    <!-- Font Awesome Free 5.7.2 -->
+
+   <script async='async' src='https://cdn.ampproject.org/v0.js'/>
+
+<script async='async' custom-element='amp-form' src='https://cdn.ampproject.org/v0/amp-form-0.1.js'/>
+
+<script async='async' custom-element='amp-sidebar' src='https://cdn.ampproject.org/v0/amp-sidebar-0.1.js'/>
+
+<script async='async' custom-element='amp-accordion' src='https://cdn.ampproject.org/v0/amp-accordion-0.1.js'/>
+
+<script async='async' custom-element='amp-iframe' src='https://cdn.ampproject.org/v0/amp-iframe-0.1.js'/>
+
+<script async='async' custom-element='amp-lightbox' src='https://cdn.ampproject.org/v0/amp-lightbox-0.1.js'/>
+
+<script src='https://cdn.plyr.io/3.6.6/plyr.polyfilled.js'/>
+
+<script src='https://cdn.plyr.io/3.6.6/plyr.js'/>
